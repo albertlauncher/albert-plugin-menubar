7bef5cbf5bafd5cca9197ac36b9e3d12dbe1de4c
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/menubar_it.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Activate</source>
+        <translation>Attiva</translation>
+    </message>
+    <message>
+        <source>The menu bar plugin requires accessibility permissions to access the menu items of the focused application.
+
+macOS requires you to enable this manually in system settings. Please toggle Albert in the accessibility settings, which will appear after you close this dialog.</source>
+        <translation>Il plugin della barra dei menu richiede i permessi di accessibilità per accedere alle voci di menu dell&apos;applicazione attiva.
+
+macOS richiede di abilitare questo manualmente nelle impostazioni di sistema. Attiva Albert nelle impostazioni di accessibilità, che appariranno dopo aver chiuso questa finestra.</translation>
+    </message>
+</context>
+</TS>
